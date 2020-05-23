aad375bee8a464a5b6d3950fde492ed9ad8c3b88
abhiroad88 <65773162+abhiroad88@users.noreply.github.com>
2020-05-23 10:06:51 +0100

    Update taskdodger.js

--- taskdodger.js
+++ taskdodger.js
@@ -1,8 +1,8 @@
 function Excuse() {
 	
 	var Dog = ['Nic', 'Matt', 'Daphne', 'Abi', 'Lottie', 'James', 'Emma', 'Katinka', 'Slack', 'Bynder', 'Carl', 'COVID-19', 'Fucking Trello', 'Photoshop', 'Illustrator', 'Premiere', 'Google Docs', 'Claudia', 'Boris'];
-	var ate = ['killed', 'peer-reviewed', 'destroyed', 'deleted', 'erased', 'remixed', 'twitched', 'recycled', 'livetweeted', 'undermined', 'underbid', 'upcycled', 'gave away', 'plagiarised', 'confiscated', 'barfed on', 'stole', 'ransomed', 'spilled water on', 'lost', 'misplaced', 'buried', 'shredded', 'hid', 'liberated', 'left with', 'ebayed', 'craigslisted', 'etsyed', 'tweeted', 'instagrammed', 'snapchatted', 'youtubed', 'dunked on', 'subteweeted', 'favorited', 'upvoted', 'downvoted', 'live cast', 'pinned', 'vlogged', 'blogged about'];
-	var myHomework = ['vibe', 'project', 'essay', 'film project', 'visualization', 'infographic', 'pre-writing', 'term paper', 'lab report', 'reading log', 'summary', 'article', 'flash cards', 'study guide', 'textbook', 'workbook', 'reading book', 'diorama', 'poster', 'dreams', 'laptop', 'computer', 'tablet', 'momentum', 'report', 'slideshow', 'slide deck', 'presentation', 'worksheet', 'handout', 'notes', 'portfolio', 'blog post', 'comment'];
+	var ate = ['killed', 'destroyed', 'deleted', 'erased', 'judged', 'stole', 'disliked', 'hated', 'gave away', 'plagiarised', 'shot down', 'barfed on', 'stole', 'spilled tea on', 'made me change', 'misplaced', 'buried', 'shredded', 'hid', 'liberated', 'left with', 'dunked on'];
+	var myHomework = ['vibe', 'task', 'idea', 'design', 'plan', 'mood', 'face', 'self-esteem', 'file', 'dreams', 'poster', 'dreams', 'laptop', 'computer', 'momentum', 'slide deck', 'presentation', 'notes', 'portfolio'];
 	
 	var who = Dog[Math.round(Math.random()*(Dog.length-1))];
 	var did = ate[Math.round(Math.random()*(ate.length-1))];
